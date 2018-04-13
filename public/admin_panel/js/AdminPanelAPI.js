e494da19482b3392313f1b94d8b97bee45eff3b1
hashmedia1 <harshit_cool90@yahoo.co.in>
2018-04-13 13:08:36 +0530

    Initial commit

---
+++ public/admin_panel/js/AdminPanelAPI.js
@@ -0,0 +1,15 @@
+var BASE_URL="https://hashmedia.co.in";
+//var BASE_URL="http://SERVER IP:9000";
+
+
+var FETCH_ALL_BUILDING = BASE_URL+"/anyplace/mapping/building/all";
+var ADD_BUILDING = BASE_URL+"/anyplace/mapping/building/add";
+var FLOOR_PLAN_UPLOAD = BASE_URL + "/anyplace/mapping/floor/upload";
+var ADD_FLOOR = BASE_URL + "/anyplace/mapping/floor/add";
+var FETCH_ALL_FLOORS = BASE_URL + "/anyplace/mapping/floor/all"
+var GET_FLOOR_IMAGE = BASE_URL + "/anyplace/floorplans64"
+var ADD_POI = BASE_URL + "/anyplace/mapping/pois/add" //TODO: Add the URL to add POI here.
+var FETCH_ALL_POI = BASE_URL + "/anyplace/mapping/pois/all_floor" //TODO: Add the URL to fetch the POIs here.
+var DELETE_BUILDING = BASE_URL + "/anyplace/mapping/building/delete"
+var DELETE_FLOOR = BASE_URL + "/anyplace/mapping/floor/delete"
+var DELETE_POI = BASE_URL + "/anyplace/mapping/pois/delete"
